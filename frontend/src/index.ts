5a2b1c80c385727597e53221b9b8729b5ee2d6e6
juicysteak7 <pedro.gonzalez6461@gmail.com>
2025-01-25 08:59:50 -0800

    getting basic routing and first index page online

---
+++ frontend/src/index.ts
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let div1 = document.getElementById('div1');
+    div1.innerHTML = '<p>This is a test element</p>';
+    console.log("page loaded");
+});
